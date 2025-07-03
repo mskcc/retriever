4bc9c27f52b3bd855f8065f1cd49ba068745618a
Nikhil Kumar <nikhilkumar516@gmail.com>
2025-07-03 11:03:15 -0400

    Add multi parsers to the parser wrapper

--- mutalyzer_retriever/parser.py
+++ mutalyzer_retriever/parser.py
@@ -8,7 +8,7 @@ def _get_reference_type(content):
         return "genbank_ncbi"
 
 
-def parse(reference_content, reference_type=None, reference_source=None):
+def parse(reference_content, reference_type=None, reference_source=None, multi=False):
 
     if reference_type is None:
         reference_type = _get_reference_type(reference_content)
@@ -16,9 +16,13 @@ def parse(reference_content, reference_type=None, reference_source=None):
     if reference_type == "lrg":
         model = lrg.parse(reference_content)
     elif reference_type == "gff3":
-        model = gff3.parse(reference_content, reference_source)
+        model = gff3.parse(reference_content, reference_source, multi=multi)
     elif reference_type == "fasta":
-        model = fasta.parse(reference_content)
+        if multi:
+            model = fasta.parse_large(reference_content)
+        else:
+            model = fasta.parse(reference_content)
+
     elif reference_type == "json":
         model = json_ensembl.parse(reference_content)
     else:
